7d0cd549a3d573c80dc1e967b2b24c99d4df3d54
Gregory Berngardt <gregoryvit@gmail.com>
2017-10-22 07:42:31 +0300

    Updated events route

--- api/v1/events.py
+++ api/v1/events.py
@@ -6,38 +6,10 @@ from . import api, login_required
 import api.common as common
 from .categories import categories
 
-events = {
-    "cinema": [
-        {
-            "id": "",
-            "title": "",
-            "image": "",
-            "category": { 'id': u'cinema', 'type': u'cinema', 'title': u'Кино'},
-            "score": 15.0,
-            "summary": "",
-            "fields": [
-                {"name": "t", "value":"a"},
-                {"name": "t", "value":"a"}
-            ]
-        }
-    ]
-}
-
-# [
-#     { 'id': u'cinema', 'type': u'cinema', 'title': u'Кино', },
-#     { 'id': u'sport', 'type': u'sport', 'title': u'Спорт', },
-#     { 'id': u'festival', 'type': u'festival', 'title': u'Фестиваль', },
-#     { 'id': u'culture', 'type': u'culture', 'title': u'Культура', },
-#     { 'id': u'humor', 'type': u'humor', 'title': u'Юмор', },
-#     { 'id': u'concert', 'type': u'concert', 'title': u'Концерт', },
-#     { 'id': u'food', 'type': u'food', 'title': u'Еда', },
-#     { 'id': u'games', 'type': u'games', 'title': u'Игры', },
-# ]
-
 import requests
 
 
-def load_kuda_go_events(category):
+def load_kuda_go_events(category, city):
     # KudaGo events list
     # GET https://kudago.com/public-api/v1.2/events/
 
@@ -46,7 +18,7 @@ def load_kuda_go_events(category):
             url="https://kudago.com/public-api/v1.2/events/",
             params={
                 "categories": category,
-                "fields": "description,price,images,id,title",
+                "fields": "description,price,images,id,title,place,age_restriction",
             },
         )
         return json.loads(response.content)
@@ -61,9 +33,10 @@ def map_event(event_dict):
 
     def get_title_value(source_dict):
         value_key = 'title'
-        if value_key not in source_dict:
+        if value_key not in source_dict or not source_dict[value_key]:
             return None
-        return source_dict[value_key]
+        value = source_dict[value_key]
+        return value[0].upper() + value[1:]
 
     def get_image_value(source_dict):
         value_key = 'images'
@@ -86,9 +59,17 @@ def map_event(event_dict):
     def get_fields_value(source_dict):
         result_fields = []
 
-        value_key = 'price'
-        if value_key in source_dict:
-            result_fields.append({'name': u'Цена', 'value': source_dict[value_key]})
+        fields_keys = [
+            ('place', u'Адрес'),
+            ('price', u'Цена'),
+            ('age_restriction', u'Возрастные ограничения'),
+            ('tags', u'Тэги'),
+            ('participants', u'Агенты'),
+        ]
+
+        for value_key, field_name in fields_keys:
+            if value_key in source_dict and source_dict[value_key]:
+                result_fields.append({'name': field_name, 'value': source_dict[value_key]})
 
         return result_fields
 
@@ -105,7 +86,7 @@ def map_event(event_dict):
 
 @api.route('/events', methods=['GET'])
 @login_required
-def get_events(user_id):
+def get_events(user_id, user):
     requested_category = request.args.get('category')
     
     cur_categories = [category for category in categories if category['id'] == requested_category]
@@ -114,7 +95,7 @@ def get_events(user_id):
 
     if cur_categories:
         category_dict = cur_categories[0]
-        kuda_go_result = load_kuda_go_events(requested_category)
+        kuda_go_result = load_kuda_go_events(requested_category, user['city'])
         if 'results' in kuda_go_result:
             kuda_go_events = kuda_go_result['results']
             print kuda_go_events
